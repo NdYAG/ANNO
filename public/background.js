f8fe7fba78b1b11c34927f5c44b0ae925515cf88
houmai <houmai@douban.com>
2014-03-23 01:24:45 +0800

    set min size

--- public/background.js
+++ public/background.js
@@ -1,5 +1,7 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('index.html', {
+    'minWidth': 660,
+    'minHeight': 580,
     'bounds': {
       'width': 960,
       'height': 600

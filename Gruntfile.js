741f99cc3ae6f461647ab2ddfb69d036e29276cf
houmai <houmai@douban.com>
2014-03-23 21:51:40 +0800

    Fix build tasks.

--- Gruntfile.js
+++ Gruntfile.js
@@ -36,8 +36,8 @@ module.exports = function(grunt) {
     concat: {
       codemirror: {
         // html mode is htmlmixed mode
-        src: ['bower_components/codemirror/codemirror.js', 'bower_components/codemirror/addon/display/placeholder.js', 'bower_components/codemirror/addon/runmode/runmode.js', 'bower_components/codemirror/mode/python/python.js', 'bower_components/codemirror/mode/commonlisp/commonlisp.js', 'bower_components/codemirror/mode/html/html.js', 'bower_components/codemirror/mode/clike/clike.js', 'bower_components/codemirror/mode/php/php.js', 'bower_components/codemirror/mode/sass/sass.js', 'bower_components/codemirror/mode/sql/sql.js', 'bower_components/codemirror/mode/perl/perl.js', 'bower_components/codemirror/mode/commonlisp/commonlisp.js'],
-        dest: 'public/lib/codemirror/codemirror.js'
+        src: ['bower_components/codemirror/lib/codemirror.js', 'bower_components/codemirror/addon/display/placeholder.js', 'bower_components/codemirror/addon/runmode/runmode.js', 'bower_components/codemirror/mode/python/python.js', 'bower_components/codemirror/mode/commonlisp/commonlisp.js', 'bower_components/codemirror/mode/html/html.js', 'bower_components/codemirror/mode/clike/clike.js', 'bower_components/codemirror/mode/php/php.js', 'bower_components/codemirror/mode/sass/sass.js', 'bower_components/codemirror/mode/sql/sql.js', 'bower_components/codemirror/mode/perl/perl.js', 'bower_components/codemirror/mode/commonlisp/commonlisp.js'],
+        dest: 'assets/lib/codemirror/codemirror.js'
       }
     },
     uglify: {
@@ -51,7 +51,7 @@ module.exports = function(grunt) {
         files: [{
           expand: true,
           cwd: "assets/js",
-          src: ["*.js", "!*.sample.js"],
+          src: ["**.js", "!*.sample.js"],
           dest: "public/js",
           ext: ".min.js"
         }]
@@ -63,8 +63,20 @@ module.exports = function(grunt) {
         files: [{
           expand: true,
           cwd: "assets/js",
-          src: ["*.js", "!*.sample.js"],
+          src: ["**.js", "!*.sample.js"],
           dest: "public/js",
+          ext: ".min.js"
+        }]
+      },
+      lib: {
+        options: {
+          mangle: false
+        },
+        files: [{
+          expand: true,
+          cwd: "assets/lib",
+          src: ["*/**.js"],
+          dest: "public/lib",
           ext: ".min.js"
         }]
       }
@@ -82,6 +94,18 @@ module.exports = function(grunt) {
         }]
       }
     },
+    copy: {
+      main: {
+        files: [
+          {
+            expand: true,
+            cwd: 'assets/resource/',
+            src: '**',
+            dest: 'public/'
+          },
+        ]
+      }
+    },
     watch: {
       jade: {
         files: ['views/partials/**/*.jade', 'views/index.jade'],
@@ -91,9 +115,17 @@ module.exports = function(grunt) {
         files: ['assets/stylus/**/*.styl'],
         tasks: ['stylus']
       },
-      js: {
+      'js': {
         files: ['assets/js/**/*.js'],
-        tasks: ['uglify:development']
+        tasks: ['uglify:develop']
+      },
+      'js:lib': {
+        files: ['assets/lib/**/*.js'],
+        tasks: ['concat', 'uglify:lib']
+      },
+      copy: {
+        files: ['assets/resource/**'],
+        tasks: ['copy']
       }
     }
   })
@@ -104,13 +136,15 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify')
   grunt.loadNpmTasks('grunt-contrib-compress')
   grunt.loadNpmTasks('grunt-contrib-watch')
+  grunt.loadNpmTasks('grunt-contrib-copy')
 
   grunt.registerTask('html', ['jade'])
   grunt.registerTask('css', ['stylus'])
-  grunt.registerTask('js', ['uglify'])
-  grunt.registerTask('js:develop', ['uglify:develop'])
-  grunt.registerTask('js:release', ['uglify:release'])
 
-  grunt.registerTask('default', ['jade', 'js:develop', 'stylus'])
-  grunt.registerTask('release', ['jade', 'js:release', 'stylus', 'compress'])
+  grunt.registerTask('js:lib', ['concat', 'uglify:lib'])
+  grunt.registerTask('js:develop', ['js:lib', 'uglify:develop'])
+  grunt.registerTask('js:release', ['js:lib', 'uglify:release'])
+
+  grunt.registerTask('default', ['jade', 'js:develop', 'stylus', 'copy'])
+  grunt.registerTask('release', ['jade', 'js:release', 'stylus', 'compress', 'copy'])
 }

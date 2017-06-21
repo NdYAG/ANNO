e33c15eb026bfc41bcc0ee287def511ca608e7b8
Eddie <daiximing199078@gmail.com>
2017-06-21 16:24:33 +0800

    filesystemService for Packaged App & Electron

--- assets/js/services/filesystemService.js
+++ assets/js/services/filesystemService.js
@@ -1,48 +1,56 @@
-// for packaged app
 app.factory('FileSystemService', [function() {
+  if (chrome.fileSystem.chooseEntry) {
+    return makeServiceForChrome()
+  } else {
+    return makeServiceForElectron()
+  }
+}])
+
+function makeServiceForChrome() {
   function errorHandler(e) {
-    console.error(e);
+    console.error(e)
   }
   function waitForIO(writer, callback) {
-    // set a watchdog to avoid eventual locking:
-    var start = Date.now();
-    // wait for a few seconds
+    var start = Date.now()
     var reentrant = function() {
       if (writer.readyState===writer.WRITING && Date.now()-start<4000) {
-        setTimeout(reentrant, 100);
-        return;
+        setTimeout(reentrant, 100)
+        return
       }
       if (writer.readyState===writer.WRITING) {
         console.error("Write operation taking too long, aborting!"+
-                      " (current writer readyState is "+writer.readyState+")");
-        writer.abort();
+                      " (current writer readyState is "+writer.readyState+")")
+        writer.abort()
       }
       else {
-        callback();
+        callback()
       }
-    };
-    setTimeout(reentrant, 100);
+    }
+    setTimeout(reentrant, 100)
   }
   function writeEntry(writableEntry, opt_blob, callback) {
 
     writableEntry.createWriter(function(writer) {
 
-      writer.onerror = errorHandler;
-      writer.onwriteend = callback;
+      writer.onerror = errorHandler
+      writer.onwriteend = callback
 
-      // If we have data, write it to the file. Otherwise, just use the file we
-      // loaded.
       if (opt_blob) {
-        writer.truncate(opt_blob.size);
+        writer.truncate(opt_blob.size)
         waitForIO(writer, function() {
-          writer.seek(0);
-          writer.write(opt_blob);
-        });
+          writer.seek(0)
+          writer.write(opt_blob)
+        })
       }
-    }, errorHandler);
+    }, errorHandler)
   }
   return {
-    'save': function(blob, filename, extension, callback) {
+    save: function(content, filename, extension, callback) {
+      var type = 'text/plain;charset=utf-8'
+      if (extension === 'html') {
+        type = 'text/html;charset=utf-8'
+      }
+      var blob = new Blob([content], { type: type })
       chrome.fileSystem.chooseEntry({
         type: 'saveFile',
         suggestedName: filename,
@@ -60,4 +68,26 @@ app.factory('FileSystemService', [function() {
       })
     }
   }
-}])
+}
+
+function makeServiceForElectron() {
+  var electron = require('electron')
+  var BrowserWindow = electron.remote.BrowserWindow
+  var dialog = electron.remote.dialog
+  var fs = require('fs')
+  return {
+    save: function (content, filename, extension, callback) {
+      dialog.showSaveDialog(BrowserWindow.getFocusedWindow(), {
+        defaultPath: filename + '.' + extension,
+      }, function(filename) {
+        if (!filename) {
+          callback(false)
+          return
+        }
+        fs.writeFile(filename, content, function(err) {
+          callback(err? false: true)
+        })
+      })
+    }
+  }
+}
